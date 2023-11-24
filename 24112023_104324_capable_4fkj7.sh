e9d5acc451ad45f4b30ad1cecb3915131fee2311
oiaaahoia <mosiademna@gmail.com>
2023-11-24 10:43:24 +0000

    Updating capable

---
+++ 24112023_104324_capable_4fkj7.sh
@@ -0,0 +1,1 @@
+French astronomer Adrien Auzout had once considered building a telescope that was 1,000 feet long in the 1600s. He thought the magnification would be so great, he would see animals on the moon
